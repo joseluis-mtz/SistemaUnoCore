54601ef8b2f12f5af9945290737d99e23b074740
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2022-08-11 20:44:21 -0500

    Modularizar alertas propias

---
+++ WebConDocs/wwwroot/js/AlertasPropias.js
@@ -0,0 +1,11 @@
+﻿function MostrarModal(Titulo = "¿Estás seguro?", Texto = "Está acción guardara la especialidad") {
+    return Swal.fire({
+        title: Titulo,
+        text: Texto,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, Confirmar'
+    })
+}
